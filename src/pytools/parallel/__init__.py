fc967bc58a646f0cd9edeb0e5826c0a7b028122c
Will Zhang <willwz@umich.edu>
2026-01-28 16:01:25 -0500

    BUG:parallel: fix import names for PExecArgList and PExecArgs

--- src/pytools/parallel/__init__.py
+++ src/pytools/parallel/__init__.py
@@ -1,3 +1,3 @@
-from ._parallel_exec import PEXEC_ARG_LIST, PEXEC_ARGS, ThreadedRunner, ThreadMethods, parallel_exec
+from ._parallel_exec import PExecArgList, PExecArgs, ThreadedRunner, ThreadMethods, parallel_exec
 
-__all__ = ["PEXEC_ARGS", "PEXEC_ARG_LIST", "ThreadMethods", "ThreadedRunner", "parallel_exec"]
+__all__ = ["PExecArgList", "PExecArgs", "ThreadMethods", "ThreadedRunner", "parallel_exec"]
