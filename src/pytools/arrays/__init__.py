ee92bb0d76b764bcb4f2c355428d74a89176d159
Will Zhang <willwz@gmail.com>
2026-09-22 17:45:31 -0400

    BUG: array: np.generic cannot have Any

--- src/pytools/arrays/__init__.py
+++ src/pytools/arrays/__init__.py
@@ -33,9 +33,7 @@ type T1[T] = tuple[T]
 type T2[T] = tuple[T, T]
 type T3[T] = tuple[T, T, T]
 
-_DType_co = TypeVar(
-    "_DType_co", bound=np.dtype[Any], covariant=True, default=np.dtype[np.generic[Any]]
-)
+_DType_co = TypeVar("_DType_co", bound=np.dtype[Any], covariant=True, default=np.dtype[np.generic])
 
 
 @runtime_checkable
