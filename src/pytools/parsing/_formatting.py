36c9fe2bb1350bfa76b3e6c1a4af286ae0d1a5bd
Will Zhang <willwz@gmail.com>
2026-09-25 11:45:53 -0400

    ENH: logging: add printing for dataclasses

--- src/pytools/parsing/_formatting.py
+++ src/pytools/parsing/_formatting.py
@@ -1,5 +1,9 @@
+import dataclasses as dc
 from collections.abc import Mapping, Sequence
-from typing import Any, cast
+from typing import TYPE_CHECKING, Any, TypeIs, cast
+
+if TYPE_CHECKING:
+    from _typeshed import DataclassInstance
 
 SCREEN_WRAP_LIMIT = 100
 TAB = "  "
@@ -40,6 +44,30 @@ def set_format(st: set[object], *, layer: int = 0) -> str:
     return head + body + tail
 
 
+def _is_dataclass_instance(obj: object) -> TypeIs[DataclassInstance]:
+    return dc.is_dataclass(obj) and not isinstance(obj, type)
+
+
+def dc_format(obj: DataclassInstance, *, layer: int = 0) -> str:
+    class_name = obj.__class__.__name__
+    items = {
+        f.name: f"{f.name}: {ppfmt(getattr(obj, f.name), layer=layer + 1)}" for f in dc.fields(obj)
+    }
+    total_len = (
+        len(class_name)
+        + 2
+        + sum(len(v) for v in items.values())
+        + 2 * (len(items) - 1)
+        + 2 * (layer + 1)
+    )
+    if total_len <= SCREEN_WRAP_LIMIT:
+        return f"{class_name}({', '.join(items.values())})"
+    head = f"{class_name}(\n"
+    body = ",\n".join([f"{TAB * (layer + 1)}{v}" for v in items.values()])
+    tail = f"\n{TAB * layer})"
+    return head + body + tail
+
+
 def ppfmt(items: object, *, layer: int = 0) -> str:
     match items:
         case str() | float() | int():
@@ -50,6 +78,8 @@ def ppfmt(items: object, *, layer: int = 0) -> str:
             return list_format(cast("Sequence[object]", items), layer=layer)
         case set():
             return set_format(cast("set[object]", items), layer=layer)
+        case _ if _is_dataclass_instance(items):
+            return dc_format(items, layer=layer)
         # case Iterable():
         #     return list_format(items, layer=layer)
         case _:
